68d665b469e86478b7ddcff4e9c944b3f3e3584a
Pratham <62070812+prathami1@users.noreply.github.com>
2020-07-30 11:14:57 -0400

    updated a few details
    updated key inputs to be easier to follow through and updated credits

--- GoogleForm.py
+++ GoogleForm.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome(r'C:\Users\iprat\Downloads\chromedriver_win32\chromedriver.exe')
+browser = webdriver.Chrome(r'path to chrome driver')
 
-browser.get('https://forms.gle/KgtRsSTeGLWvaDo98')
+browser.get('url of the form')
 firstName = browser.find_element_by_name("First Name?");
 lastName = browser.find_element_by_name("Last Name?");
 reflection = browser.find_element_by_name("Reflect on what you did");
@@ -22,3 +22,5 @@ reflection.send_keys("Keys.ENTER");
 # submitting the form
 submission = browser.find_element_by_link_text("Submit");
 submission.click();
+
+# written by Pratham Inamdar, implementing selenium through Python
